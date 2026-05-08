02ce773c9f10ba9652a21ebac3ba01ec7b10d8c8
MidoriKurage <kuragectl@gmail.com>
2026-05-08 18:18:26 +0800

    feat(server/oidc): Add OIDC redirect, login and signup endpoints

--- server/src/oauth2/oauth2.module.ts
+++ server/src/oauth2/oauth2.module.ts
@@ -9,9 +9,12 @@ import OAuth2Service from "./oauth2.service";
 import GoogleCalendarEvents from "./google-calendar-events.entity";
 import GoogleCalendarCreatedEvent from "./google-calendar-created-event.entity";
 import MicrosoftOAuth2 from "./microsoft-oauth2.entity";
+import GenericOAuth2 from "./generic-oauth2.entity";
 import MicrosoftCalendarEvents from "./microsoft-calendar-events.entity";
 import MicrosoftCalendarCreatedEvent from "./microsoft-calendar-created-event.entity";
 import CacherModule from "../cacher/cacher.module";
+import OidcDiscoveryService from "./oidc-discovery.service";
+import OidcTokenVerifier from "./oidc-token-verifier";
 
 @Module({
   imports: [
@@ -26,9 +29,10 @@ import CacherModule from "../cacher/cacher.module";
       MicrosoftOAuth2,
       MicrosoftCalendarEvents,
       MicrosoftCalendarCreatedEvent,
+      GenericOAuth2,
     ]),
   ],
-  providers: [OAuth2Service],
+  providers: [OAuth2Service, OidcDiscoveryService, OidcTokenVerifier],
   exports: [OAuth2Service],
   controllers: [Oauth2Controller],
 })

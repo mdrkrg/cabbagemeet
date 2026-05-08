1feef70c20316f201782eb5b261347232e48ff2d
MidoriKurage <kuragectl@gmail.com>
2026-05-08 17:44:00 +0800

    feat(server/oidc): Add GENERIC_OIDC enum and add GenericOAuth2 entity

--- server/src/oauth2/oauth2-common.ts
+++ server/src/oauth2/oauth2-common.ts
@@ -1,85 +1,82 @@
-import { capitalize } from '../misc.utils';
-import type GoogleCalendarCreatedEvent from './google-calendar-created-event.entity';
-import type MicrosoftCalendarCreatedEvent from './microsoft-calendar-created-event.entity';
+import { capitalize } from "../misc.utils";
+import type GoogleCalendarCreatedEvent from "./google-calendar-created-event.entity";
+import type MicrosoftCalendarCreatedEvent from "./microsoft-calendar-created-event.entity";
 
-export const oidcScopes = ['openid', 'profile', 'email'] as const;
+export const oidcScopes = ["openid", "profile", "email"] as const;
 // NOTE: if you add a new provider, make sure to update server-info-response.ts
 export enum OAuth2ProviderType {
   GOOGLE = 1,
   MICROSOFT,
+  GENERIC_OIDC,
 }
 // Need to remove strings because the TS compiler will also map the enum values
 // to their names (basically a bidirectional map)
 // See https://stackoverflow.com/a/39439520
 export const oauth2ProviderTypes = Object.values(OAuth2ProviderType).filter(
-  (val) => typeof val !== 'string',
+  (val) => typeof val !== "string",
 ) as OAuth2ProviderType[];
 export const oauth2ProviderNames = (
-  Object.values(OAuth2ProviderType).filter(
-    (val) => typeof val === 'string',
-  ) as string[]
+  Object.values(OAuth2ProviderType).filter((val) => typeof val === "string") as string[]
 ).map(capitalize);
-export const oauth2ProviderNamesMap: Record<OAuth2ProviderType, string> =
-  Object.entries(OAuth2ProviderType)
-    .filter(([key, val]) => typeof val === 'string')
-    .reduce(
-      (o, [providerType, providerNameUpper]) => ({
-        ...o,
-        [providerType]: capitalize(providerNameUpper as string),
-      }),
-      {} as Record<OAuth2ProviderType, string>,
-    );
-export const oauth2TableNames = oauth2ProviderNames.map(
-  (name) => `${name}OAuth2`,
-);
-export const oauth2TableNamesMap = Object.entries(
-  oauth2ProviderNamesMap,
-).reduce(
+export const oauth2ProviderNamesMap: Record<OAuth2ProviderType, string> = Object.entries(
+  OAuth2ProviderType,
+)
+  .filter(([key, val]) => typeof val === "string")
+  .reduce(
+    (o, [providerType, providerNameUpper]) => ({
+      ...o,
+      [providerType]: capitalize(providerNameUpper as string),
+    }),
+    {} as Record<OAuth2ProviderType, string>,
+  );
+export const oauth2TableNames = oauth2ProviderNames.map((name) => `${name}OAuth2`);
+export const oauth2TableNamesMap = Object.entries(oauth2ProviderNamesMap).reduce(
   (o, [key, val]) => ({ ...o, [key]: `${val}OAuth2` }),
   {} as Record<OAuth2ProviderType, string>,
 );
-export const oauth2CreatedEventTableNamesMap = Object.entries(
-  oauth2ProviderNamesMap,
-).reduce(
+export const oauth2CreatedEventTableNamesMap = Object.entries(oauth2ProviderNamesMap).reduce(
   (o, [key, val]) => ({ ...o, [key]: `${val}CalendarCreatedEvent` }),
   {} as Record<OAuth2ProviderType, string>,
 );
-export const oauth2Reasons = ['link', 'signup', 'login'] as const;
+export const oauth2Reasons = ["link", "signup", "login"] as const;
 
 export class OAuth2NotConfiguredError extends Error {
   constructor() {
-    super('OAuth2 not configured');
+    super("OAuth2 not configured");
   }
 }
 export class OAuth2ErrorResponseError extends Error {
-  constructor(public statusCode: number, public errorCode?: string) {
+  constructor(
+    public statusCode: number,
+    public errorCode?: string,
+  ) {
     super();
   }
 }
 // TODO: replace these with a single class and an enum argument
 export class OAuth2InvalidStateError extends Error {
   constructor() {
-    super('OAuth2 invalid state');
+    super("OAuth2 invalid state");
   }
 }
 export class OAuth2InvalidOrExpiredNonceError extends Error {
   constructor() {
-    super('OAuth2 invalid or expired nonce');
+    super("OAuth2 invalid or expired nonce");
   }
 }
 export class OAuth2NoRefreshTokenError extends Error {
   constructor() {
-    super('OAuth2 no refresh token');
+    super("OAuth2 no refresh token");
   }
 }
 export class OAuth2NotAllScopesGrantedError extends Error {
   constructor() {
-    super('OAuth2 not all scopes granted');
+    super("OAuth2 not all scopes granted");
   }
 }
 export class OAuth2AccountAlreadyLinkedError extends Error {
   constructor() {
-    super('OAuth2 account already linked');
+    super("OAuth2 account already linked");
   }
 }
 

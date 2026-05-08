c26870894f98dc3688945bc362b826990d1fb393
MidoriKurage <kuragectl@gmail.com>
2026-05-08 09:46:14 +0800

    fix(server/lint): Resolve server linting issues

--- server/src/misc.utils.ts
+++ server/src/misc.utils.ts
@@ -1,23 +1,23 @@
-import { sign as jwtSignCb } from 'jsonwebtoken';
+import jwt, { sign as jwtSignCb } from "jsonwebtoken";
 
 export function assert(condition: any, message?: string): asserts condition {
   if (!condition) {
-    throw new Error(message ?? 'Assertion failed');
+    throw new Error(message ?? "Assertion failed");
   }
 }
 
-export function assertIsNever(x: never): never {
+export function assertIsNever(_: never): never {
   throw new Error();
 }
 
 export function encodeQueryParams(params: Record<string, string>): string {
   return Object.entries(params)
-    .map(([key, value]) => key + '=' + encodeURIComponent(value))
-    .join('&');
+    .map(([key, value]) => key + "=" + encodeURIComponent(value))
+    .join("&");
 }
 
 export function stripTrailingSlash(s: string): string {
-  if (s.endsWith('/')) {
+  if (s.endsWith("/")) {
     return s.substring(0, s.length - 1);
   }
   return s;
@@ -37,12 +37,12 @@ export function capitalize(s: string): string {
 }
 
 export function jwtSign(
-  payload: Parameters<typeof jwtSignCb>[0],
-  secretOrPrivateKey: Parameters<typeof jwtSignCb>[1],
-  options?: Parameters<typeof jwtSignCb>[2],
+  payload: string | Buffer | object,
+  secretOrPrivateKey: jwt.Secret,
+  options?: jwt.SignOptions,
 ): Promise<string> {
   return new Promise((resolve, reject) => {
-    jwtSignCb(payload, secretOrPrivateKey, options, (err, token) => {
+    jwtSignCb(payload, secretOrPrivateKey, options as any, (err: any, token: any) => {
       if (err) {
         reject(err);
       } else {
